13dc8def3b58b43ca91c08072b78590655289353
woong <ung91@yahoo.com>
2021-09-29 20:02:22 +0900

    chat 메세지 전송 및 저장 구현

--- js/chat.js
+++ js/chat.js
@@ -1,26 +1,57 @@
-function getMsg(msg){
+const eventSource = new EventSource("http://localhost:8080/sender/leo/receiver/bob");
+
+eventSource.onmessage = (e) =>{
+    const data = JSON.parse(e.data);
+}
+
+// 메세지 박스
+function getSendMsgBox(msg, time){
     return `<div class="sent_msg">
                 <p>${msg}</p>
-                <span class="time_date"> 11:18 | Today</span>
+                <span class="time_date">${time}</span>
             </div>`;
 }
 
-function makeMsg(){
+// 메세지 생성
+async function makeMsg(){
     let chatBox = document.querySelector("#chat-box");
     let msgInput = document.querySelector("#chat-outgoing-msg");
 
     let chatOutgoingBox = document.createElement("div");
     chatOutgoingBox.className = "outgoing_msg";
 
-    chatOutgoingBox.innerHTML = getMsg(msgInput.value);
+    let date = new Date();
+    let now = date.getHours() + ":" + date.getMinutes() + "|" + date.getMonth() + "/" + date.getDate();
+
+    let chat = {
+        sender: "leo",
+        receiver: "bob",
+        msg: msgInput.value
+    };
+
+    let response = await fetch("http://localhost:8080/chat", {
+        method: "post",
+        body: JSON.stringify(chat),
+        headers:{
+            "Content-Type":"application/json; charset=utf-8"
+        }
+    });
+
+    console.log(response);
+    let parseResponse = await response.json();
+    console.log(parseResponse);
+
+    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value, now);
     chatBox.append(chatOutgoingBox);
     msgInput.value = "";
 }
 
+// 보내기 버튼 클릭
 document.querySelector("#chat-outgoing-button").addEventListener("click", ()=>{
     makeMsg();
 });
 
+// 보내기 엔터
 document.querySelector("#chat-outgoing-msg").addEventListener("keydown", (e)=>{
     if(e.keyCode == 13){
         makeMsg();
